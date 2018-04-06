c709a780486e550751904bba6f06db540ee29d0b
Kira Yang <wenqin.z.yang@gmail.com>
2018-04-06 18:04:42 -0400

    Initial commit

---
+++ redux-basics.js
@@ -0,0 +1,51 @@
+const redux=require('redux');
+const createStore =redux.createStore;
+
+const initialState = {
+    counter: 0
+}
+//Reducer
+const rootReducer = (state = initialState, action) => {
+    if (action.type === "INC_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === "ADD_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
+    if (action.type === "DEC_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === "SUB_COUNTER") {
+        return {
+            ...state,
+            counter: state.counter - action.value
+        }
+    }
+    return state;
+};
+
+//Store
+const store =createStore(rootReducer);
+console.log(store.getState());
+
+//Subscription
+store.subscribe(() => {
+    console.log('subscription: ', store.getState())
+})
+
+//Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+store.dispatch({type: 'DEC_COUNTER'});
+store.dispatch({type: 'SUB_COUNTER', value: 10});
+console.log(store.getState());
+
